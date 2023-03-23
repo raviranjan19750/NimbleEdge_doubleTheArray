9a35143daf11d3e7efaa0c777277e285bbe41a65
raviranjan19750 <ranjan.ranjan19750@gmail.com>
2023-03-24 01:18:24 +0530

    sdk test case added

--- doubleThisArray/src/test/java/com/example/doublethisarray/ExampleUnitTest.kt
+++ doubleThisArray/src/test/java/com/example/doublethisarray/ExampleUnitTest.kt
@@ -1,17 +1,27 @@
 package com.example.doublethisarray
 
+import org.junit.Assert.assertArrayEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class DoubleThisArrayTest {
+
+    companion object{
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testDoubleThisArray() {
+        val arrayClass = DoubleThisArray()
+        val expectedOutput = floatArrayOf(2.0f, 4.0f,6.0f, 8.0f)
+        val actualOutput = arrayClass.doubleTheArray(floatArrayOf(1.0f, 2.0f, 3.0f, 4.0f))
+        assertArrayEquals(expectedOutput, actualOutput, 0.0f)
     }
 }
